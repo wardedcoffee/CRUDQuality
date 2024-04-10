bbc36e9439a2140a51b327e6e0f34f66bdb7ed8f
Marcos Nascimento <marcozzymandias@gmail.com>
2024-04-09 22:27:20 -0300

    A4.4: Implementa busca e primeiro generics
    A4.4: Implementando nossa busca e seu primeiro generics no TypeScript

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -4,13 +4,26 @@ interface TodoControllerGetParams {
   page: number;
 }
 async function get( params: TodoControllerGetParams) {
-  //console.log(params);
   return todoRepository.get({
     page: params.page,
     limit: 2,
   });
 }
 
+function filterTodosByContent<Todo>(
+  search: string,
+  todos: Array<Todo & { content: string }>
+): Array<Todo> {
+  const homeTodos = todos.filter((todo) => {
+    const searchNormalized = search.toLowerCase();
+    const contentNormalized = todo.content.toLowerCase();
+    return contentNormalized.includes(searchNormalized);
+  });
+
+  return homeTodos;
+}
+
 export const todoController = {
   get,
+  filterTodosByContent,
 };
